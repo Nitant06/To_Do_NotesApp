000cc575d81ae63d55234a33f14f27fb748bb638
Nitant06 <30805667+Nitant06@users.noreply.github.com>
2020-12-23 16:56:30 +0530

    Initial commit

---
+++ app/src/main/java/com/nitant/notesapp/NotesRVAdapter.kt
@@ -0,0 +1,55 @@
+package com.nitant.notesapp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NotesRVAdapter(val context: Context,var listener:INotesRVAdapter):
+    RecyclerView.Adapter<NotesRVAdapter.NoteViewHolder>() {
+
+    var allNotes = ArrayList<Note>()
+
+    inner class NoteViewHolder(itemView:View):RecyclerView.ViewHolder(itemView) {
+
+        var textView = itemView.findViewById<TextView>(R.id.text)
+        var deleteButton = itemView.findViewById<ImageView>(R.id.deleteButton)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+       val viewHolder = NoteViewHolder(LayoutInflater.from(context).inflate(R.layout.item_note,parent,false))
+       viewHolder.deleteButton.setOnClickListener{
+           listener.onItemClicked(allNotes[viewHolder.adapterPosition])
+       }
+
+        return viewHolder
+    }
+
+    override fun getItemCount(): Int {
+        return allNotes.size
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val currentNote = allNotes[position]
+        holder.textView.text = currentNote.text
+    }
+
+    fun updateList(newList:List<Note>){
+
+        allNotes.clear()
+        allNotes.addAll(newList)
+
+        notifyDataSetChanged()
+    }
+
+}
+
+interface INotesRVAdapter {
+
+    fun onItemClicked(note:Note)
+
+}
